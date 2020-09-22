e0bd7950cca03df195042cbfe4e0f002ed74d5cb
ckgathi <ckgathi@users.noreply.github.com>
2020-09-22 16:05:17 +0200

    locator model wrappers

--- flourish_dashboard/model_wrappers/maternal_locator_model_wrapper.py
+++ flourish_dashboard/model_wrappers/maternal_locator_model_wrapper.py
@@ -2,7 +2,7 @@ from django.apps import apps as django_apps
 from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
 
-from edc_model_wrapper import ModelWrapper
+from edc_model_wrapper import ModelWrapper 
 
 
 class MaternalLocatorModelWrapperMixin:
