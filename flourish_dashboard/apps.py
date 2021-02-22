3849bf45a901ab69b0529f32e5ebad4d82a5c992
ckgathi <ckgathi@users.noreply.github.com>
2021-02-23 00:26:17 +0200

    removee create locator group

--- flourish_dashboard/apps.py
+++ flourish_dashboard/apps.py
@@ -5,10 +5,3 @@ from django.apps import AppConfig as DjangoAppConfig
 class AppConfig(DjangoAppConfig):
     name = 'flourish_dashboard'
     admin_site_name = 'flourish_caregiver_admin'
-
-    def ready(self):
-        from django.contrib.auth.models import Group
-        try:
-            Group.objects.get(name='locator users')
-        except Group.DoesNotExist:
-            Group.objects.create(name='locator users')
