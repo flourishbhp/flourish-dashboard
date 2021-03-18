8aef3c2ac8162a3a026e55d41a9e0bb0f4402cb6
ame diphoko <diphokoame@gmail.com>
2021-03-18 20:07:53 +0200

    add child age to dummy consent model wrapper

--- flourish_dashboard/model_wrappers/child_dummy_consent_model_wrapper.py
+++ flourish_dashboard/model_wrappers/child_dummy_consent_model_wrapper.py
@@ -1,4 +1,6 @@
+from dateutil.relativedelta import relativedelta
 from django.conf import settings
+from edc_base.utils import get_utcnow
 from edc_model_wrapper import ModelWrapper
 
 from .child_assent_model_wrapper_mixin import ChildAssentModelWrapperMixin
@@ -47,3 +49,15 @@ class ChildDummyConsentModelWrapper(ChildAssentModelWrapperMixin,
         subject_identifier.pop()
         caregiver_subject_identifier = '-'.join(subject_identifier)
         return caregiver_subject_identifier
+
+    @property
+    def child_age(self):
+        if getattr(self, 'assent_model_obj'):
+            birth_date = self.assent_model_obj.dob
+            difference = relativedelta(get_utcnow().date(), birth_date)
+            months = 0
+            if difference.years > 0:
+                months = difference.years * 12
+            years = round((months + difference.months) / 12, 2)
+            return years
+        return 0
