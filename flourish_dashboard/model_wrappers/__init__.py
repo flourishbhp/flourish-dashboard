0e77c1f25bc3f5dbd22c58f09171eeca9ab0c258
ame diphoko <diphokoame@gmail.com>
2021-03-18 19:50:12 +0200

    add caregiver child consent model wrapper

--- flourish_dashboard/model_wrappers/__init__.py
+++ flourish_dashboard/model_wrappers/__init__.py
@@ -1,6 +1,8 @@
 from .action_item_model_wrapper import ActionItemModelWrapper
 from .appointment_model_wrapper import AppointmentModelWrapper
+from .caregiver_child_consent_model_wrapper import CaregiverChildConsentModelWrapper
 from .child_appointment_model_wrapper import ChildAppointmentModelWrapper
+from .child_assent_model_wrapper import ChildAssentModelWrapper
 from .child_crf_model_wrapper import ChildCrfModelWrapper
 from .child_dummy_consent_model_wrapper import ChildDummyConsentModelWrapper
 from .child_offstudy_model_wrapper import ChildOffstudyModelWrapper
@@ -13,4 +15,3 @@ from .caregiver_locator_model_wrapper import CaregiverLocatorModelWrapper
 from .maternal_screening_model_wrapper import MaternalScreeningModelWrapper
 from .maternal_visit_model_wrapper import MaternalVisitModelWrapper
 from .subject_consent_model_wrapper import SubjectConsentModelWrapper
-from .caregiver_locator_model_wrapper import CaregiverLocatorModelWrapper
