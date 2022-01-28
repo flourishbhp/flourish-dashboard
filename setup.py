38ca954d7e5499df258fc4f69f50cfa040718b0c
Itumeleng Mosweu <imosweuk@gmail.com>
2022-01-28 08:53:36 +0200

    bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='flourish-dashboard',
-    version='0.1.21',
+    version='0.1.22',
     author=u'Software Engineering & Data Management',
     author_email='se-dmc@bhp.org.bw',
     packages=find_packages(),
