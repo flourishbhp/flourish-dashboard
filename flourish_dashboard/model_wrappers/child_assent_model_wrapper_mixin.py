b88bb54cce725dcc411125f3d6770bd8472d518a
nmunatsibw <mcturnergarai@gmail.com>
2023-04-20 16:14:28 +0200

    feat(Pre flourish): heu huu matching functionality

--- flourish_dashboard/model_wrappers/child_assent_model_wrapper_mixin.py
+++ flourish_dashboard/model_wrappers/child_assent_model_wrapper_mixin.py
@@ -24,6 +24,10 @@ class ChildAssentModelWrapperMixin:
         return django_apps.get_model('flourish_caregiver.subjectconsent')
 
     @property
+    def heu_huu_match_cls(self):
+        return django_apps.get_model('pre_flourish.heuhuumatch')
+
+    @property
     def assent_model_obj(self):
         """Returns a child assent model instance or None.
         """
@@ -38,7 +42,6 @@ class ChildAssentModelWrapperMixin:
         """"Returns a wrapped saved or unsaved child assent
         """
         if self.child_age >= 7:
-
             model_obj = self.assent_model_obj or self.assent_model_cls(
                 **self.create_child_assent_options(self.caregiverchildconsent_obj))
 
@@ -95,10 +98,21 @@ class ChildAssentModelWrapperMixin:
                 model_obj = self.child_assent_model_obj(caregiverchildconsent) or \
                             self.assent_model_cls(
                                 **self.create_child_assent_options(caregiverchildconsent))
-                # create options based on caregiverchildconsent, which is either version 1 or version 2
+                # create options based on caregiverchildconsent, which is either
+                # version 1 or version 2
 
                 wrapped_entries.append(ChildAssentModelWrapper(model_obj))
         return wrapped_entries
+
+    @property
+    def huu_participant(self):
+        try:
+            heu_huu_match_obj = self.heu_huu_match_cls.objects.get(
+                heu_prt=self.subject_identifier)
+        except self.heu_huu_match_cls.DoesNotExist:
+            return None
+        else:
+            return heu_huu_match_obj.huu_prt
 
     def child_assents_exists(self) -> bool:
 
--- flourish_dashboard/templatetags/flourish_dashboard_extras.py
+++ flourish_dashboard/templatetags/flourish_dashboard_extras.py
@@ -39,6 +39,15 @@ def child_dashboard_button(model_wrapper):
         child_dashboard_url=child_dashboard_url,
         subject_identifier=model_wrapper.subject_identifier)
 
+@register.inclusion_tag(
+    'flourish_dashboard/buttons/child_dashboard_button.html')
+def huu_match_child_dashboard_button(model_wrapper):
+    child_dashboard_url = settings.DASHBOARD_URL_NAMES.get(
+        'pre_flourish_child_dashboard_url')
+    return dict(
+        child_dashboard_url=child_dashboard_url,
+        subject_identifier=model_wrapper.huu_participant)
+
 
 @register.inclusion_tag('flourish_dashboard/buttons/eligibility_button.html')
 def eligibility_button(model_wrapper):
