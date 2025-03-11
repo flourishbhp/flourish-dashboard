5bde9f9200b66bd8a43da12e79bd8355c652c75d
Ame Diphoko <diphokoame@gmail.com>
2025-03-11 07:05:55 +0200

    bump version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='flourish-dashboard',
-    version='0.2.20',
+    version='0.2.21',
     author=u'Software Engineering & Data Management',
     author_email='se-dmc@bhp.org.bw',
     packages=find_packages(),
