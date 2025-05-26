8709cefadb795d77940f74ee3767478aaef8da1c
Ame Diphoko <diphokoame@gmail.com>
2025-05-26 12:39:39 +0200

    update: refactor code, allow offstudy eligibility status to show on listboard

--- flourish_dashboard/model_wrappers/child_dummy_consent_model_wrapper.py
+++ flourish_dashboard/model_wrappers/child_dummy_consent_model_wrapper.py
@@ -1,5 +1,13 @@
+from math import modf
+from datetime import date
+from dateutil.relativedelta import relativedelta
+
+from django.apps import apps as django_apps
 from django.conf import settings
+from edc_base.utils import get_utcnow
 from edc_model_wrapper import ModelWrapper
+
+from flourish_caregiver.helper_classes.utils import cohort_assigned
 
 from .child_assent_model_wrapper_mixin import ChildAssentModelWrapperMixin
 from .child_birth_model_wrapper_mixin import ChildBirthModelWrapperMixin
@@ -23,3 +31,68 @@ class ChildDummyConsentModelWrapper(ChildDummyConsentModelWrapperMixin,
         'child_listboard_url')
     next_url_attrs = ['subject_identifier', 'screening_identifier']
     querystring_attrs = ['subject_identifier', 'screening_identifier']
+
+    def has_fu_visit(self, cohort_name):
+        cs_model_cls = django_apps.get_model(
+            'flourish_caregiver.cohortschedules')
+
+        schedule_names = cs_model_cls.objects.filter(
+            cohort_name=cohort_name,
+            schedule_type__in=['followup', 'sq_followup'],
+            child_count__isnull=True).values_list('schedule_name', flat=True)
+
+        visit_model_cls = django_apps.get_model(
+            f'flourish_child.childvisit')
+
+        return visit_model_cls.objects.filter(
+            subject_identifier=self.object.subject_identifier,
+            schedule_name__in=schedule_names).exists()
+
+    @property
+    def eligible_for_protocol_completion(self):
+        is_eligible = True
+        cohort_upper = {
+            'cohort_a': 5.0833,
+            'cohort_a_sec': 5.0833,
+            'cohort_b': 10.0833,
+            'cohort_b_sec': 10.0833}
+
+        current_cohort = self.cohort_model_obj({
+            'subject_identifier': self.object.subject_identifier,
+            'current_cohort': True, })
+        cohort_name = current_cohort.name
+        study_child_id = self.child_consent.study_child_identifier
+        child_dob = self.child_consent.child_dob
+
+        today = get_utcnow().date()
+        cutoff_date = date(2025, 6, 20)
+
+        _cohort = cohort_assigned(study_child_id, child_dob, today)
+        if _cohort and _cohort != cohort_name:
+            cohort_name = _cohort
+
+        age_now = current_cohort.child_age
+        cohort_age = cohort_upper.get(current_cohort.name, None)
+        if cohort_age and age_now < cohort_age:
+            age_diff = cohort_age - age_now
+
+            fractional_year, whole_years = modf(age_diff)
+            whole_years = int(whole_years)
+            months = int(round(fractional_year * 12))
+
+            projected_date = today + relativedelta(years=whole_years, months=months)
+            projected_cohort = None
+            if projected_date > today and projected_date < cutoff_date:
+                projected_cohort = cohort_assigned(
+                    study_child_id, child_dob, projected_date)
+
+            if projected_cohort and projected_cohort != cohort_name:
+                cohort_name = projected_cohort
+
+        if 'sec' not in cohort_name and not self.has_fu_visit(cohort_name):
+            is_eligible = False
+            if cohort_name == 'cohort_a':
+                age_at_cutoff = current_cohort.child_age_at_date(cutoff_date)
+                is_eligible = (age_at_cutoff * 12) < 18
+
+        return is_eligible
--- flourish_dashboard/views/child_subject/dashboard/dashboard_view.py
+++ flourish_dashboard/views/child_subject/dashboard/dashboard_view.py
@@ -1,5 +1,3 @@
-from math import modf
-from datetime import date
 from dateutil.relativedelta import relativedelta
 from django.apps import apps as django_apps
 from django.conf import settings
@@ -19,7 +17,6 @@ from edc_subject_dashboard.view_mixins import SubjectDashboardViewMixin
 from edc_visit_schedule.site_visit_schedules import site_visit_schedules
 
 from flourish_caregiver.helper_classes import MaternalStatusHelper
-from flourish_caregiver.helper_classes.utils import cohort_assigned
 from flourish_child.helper_classes.brain_ultrasound_helper import BrainUltrasoundHelper
 from flourish_child.helper_classes.child_fu_onschedule_helper import \
     ChildFollowUpEnrolmentHelper
@@ -398,7 +395,12 @@ class DashboardView(DashboardViewMixin, EdcBaseViewMixin, SubjectDashboardViewMi
 
             self.check_ageing_out()
 
-            self.eligible_for_protocol_completion()
+            offstudy_eligible = self.consent_wrapped.eligible_for_protocol_completion
+            if offstudy_eligible:
+                messages.info(
+                    self.request,
+                    'Please note, this child is eligible for '
+                    'off-study/protocol completion.')
 
         child_visit_cls = django_apps.get_model('flourish_child.childvisit')
 
@@ -655,67 +657,3 @@ class DashboardView(DashboardViewMixin, EdcBaseViewMixin, SubjectDashboardViewMi
         """Returns True if the child is enrolled on the brain ultrasound schedule."""
         return (self.brain_ultrasound_helper.is_enrolled_brain_ultrasound() and
                 not self.brain_ultrasound_helper.is_onschedule)
-
-    def has_fu_visit(self, cohort_name):
-        cs_model_cls = django_apps.get_model(
-            'flourish_caregiver.cohortschedules')
-
-        schedule_names = cs_model_cls.objects.filter(
-            cohort_name=cohort_name,
-            schedule_type__in=['followup', 'sq_followup'],
-            child_count__isnull=True).values_list('schedule_name', flat=True)
-
-        visit_model_cls = django_apps.get_model(
-            f'flourish_child.{self.visit_attr}')
-
-        return visit_model_cls.objects.filter(
-            subject_identifier=self.subject_identifier,
-            schedule_name__in=schedule_names).exists()
-
-    def eligible_for_protocol_completion(self):
-        is_eligible = True
-        cohort_upper = {
-            'cohort_a': 5.0833,
-            'cohort_a_sec': 5.0833,
-            'cohort_b': 10.0833,
-            'cohort_b_sec': 10.0833}
-
-        current_cohort = self.consent_wrapped.cohort_model_obj({
-            'subject_identifier': self.subject_identifier,
-            'current_cohort': True, })
-        cohort_name = current_cohort.name
-        study_child_id = self.caregiver_child_consent.study_child_identifier
-        child_dob = self.caregiver_child_consent.object.child_dob
-
-        today = get_utcnow().date()
-        cutoff_date = date(2025, 6, 30)
-
-        _cohort = cohort_assigned(study_child_id, child_dob, today)
-        if (_cohort and 'sec' not in _cohort) and _cohort != cohort_name:
-            cohort_name = _cohort
-
-        age_now = current_cohort.child_age
-        cohort_age = cohort_upper.get(current_cohort.name, None)
-        if cohort_age and age_now < cohort_age:
-            age_diff = cohort_age - age_now
-
-            fractional_year, whole_years = modf(age_diff)
-            whole_years = int(whole_years)
-            months = int(round(fractional_year * 12))
-
-            projected_date = today + relativedelta(years=whole_years, months=months)
-            projected_cohort = None
-            if projected_date > today and projected_date < cutoff_date:
-                projected_cohort = cohort_assigned(study_child_id, child_dob, projected_date)
-
-            if (projected_cohort and 'sec' not in projected_cohort) and projected_cohort != cohort_name:
-                cohort_name = projected_cohort
-
-        if (cohort_name in ['cohort_b', 'cohort_c'] and
-                not self.has_fu_visit(cohort_name)):
-            is_eligible = False
-
-        if is_eligible:
-            messages.info(
-                self.request,
-                'Please note, this child is eligible for off-study/protocol completion.')
